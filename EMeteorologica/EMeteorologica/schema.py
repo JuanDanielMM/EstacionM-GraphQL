bf7c264d5cc25209886c3fc25ad1da5e71d168d3
JuanDanielMM <juand16mm@hotmail.com>
2021-11-09 19:35:05 -0600

    Mutaciones creadas #2

--- EMeteorologica/EMeteorologica/schema.py
+++ EMeteorologica/EMeteorologica/schema.py
@@ -6,5 +6,7 @@ import mediciones.schema
 class Query(mediciones.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(mediciones.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
